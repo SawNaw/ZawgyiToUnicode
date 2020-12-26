a1f4fea9fa0d836186730e06c9aa43ffc0e495fe
Saw Naw <waterchan@gmail.com>
2020-12-26 19:18:07 +0000

    Implement single file renaming

---
+++ README.md
@@ -0,0 +1,4 @@
+# ZawgyiToUnicode
+Text and batch filename conversion between Zawgyi and Unicode-compliant Burmese fonts.
+
+Credit to GitHub user mgthantzin. I have taken and modified the code from his repo: https://github.com/mgthantzin/mmfont.Net/tree/master/mmfont.Net
---
+++ ZawgyiToUnicode.StringConverter/Convert.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.RegularExpressions;
+
+namespace ZawgyiToUnicode.StringConverter;
+
+public class Convert
+{
+    public static string? ToZawgyi(string unicodeText) => DoConversion(unicodeText, Rules.UnicodeToZawgyiRules);
+    public static string? ToUnicode(string zawgyiText) => DoConversion(zawgyiText, Rules.ZawgyiToUnicodeRules);
+
+    private static string DoConversion(string input, IReadOnlyDictionary<string, string> conversionRules)
+    {
+        foreach (var rule in conversionRules)
+        {
+            Regex rgx = new(rule.Key);
+            input = rgx.Replace(input, rule.Value);
+        }
+
+        return input;
+    }
+}
