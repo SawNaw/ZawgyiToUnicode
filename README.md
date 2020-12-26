c8f8e69888be80c11e905581556c8ab4e203b960
Saw Naw <waterchan@gmail.com>
2020-12-26 19:18:07 +0000

    Implement string converter

---
+++ README.md
@@ -0,0 +1,6 @@
+# ZawgyiToUnicode
+Text and batch filename conversion between Zawgyi and Unicode-compliant Burmese fonts.
+
+Credit to GitHub user mgthantzin for the converter code. This is his repo: https://github.com/mgthantzin/mmfont.Net/tree/master/mmfont.Net
+
+I took the code, gave it some touchup, and added unit tests. I plan to add batch filename conversion.
---
+++ Tests/ZawgyiToUnicode.Converter.Tests/ConvertTests/ToUnicodeTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace ZawgyiToUnicode.Converter.Tests.ConvertTests;
+public class ToUnicodeTests
+{
+    [TestCase("တပ်မက်မှု ‘တဏှာ’ ချုပ်ရ၏၊ တပ်မက်မှု ‘တဏှာ’ ချုပ်ခြင်းကြောင့် ပြင်း စွာ စွဲလမ်းမှု ‘ဥပါဒါန်’ ချုပ်ရ၏၊ ပြင်းစွာ စွဲလမ်းမှု ‘ဥပါဒါန်’ ချုပ်ခြင်းကြောင့် ဘဝ ချုပ်ရ၏၊ ဘဝ ချုပ်ခြင်း ကြောင့် ပဋိသန္ဓေနေမှု ‘ဇာတိ’ ချုပ်ရ၏၊ ဇာတိ ချုပ်ခြင်းကြောင့် အိုမှု ‘ဇရာ’၊ သေမှု ‘မရဏ’၊ စိုးရိမ်မှု",
+          "တပ္မက္မႈ ‘တဏွာ’ ခ်ဳပ္ရ၏၊ တပ္မက္မႈ ‘တဏွာ’ ခ်ဳပ္ျခင္းေၾကာင့္ ျပင္း စြာ စြဲလမ္းမႈ ‘ဥပါဒါန္’ ခ်ဳပ္ရ၏၊ ျပင္းစြာ စြဲလမ္းမႈ ‘ဥပါဒါန္’ ခ်ဳပ္ျခင္းေၾကာင့္ ဘဝ ခ်ဳပ္ရ၏၊ ဘဝ ခ်ဳပ္ျခင္း ေၾကာင့္ ပဋိသေႏၶေနမႈ ‘ဇာတိ’ ခ်ဳပ္ရ၏၊ ဇာတိ ခ်ဳပ္ျခင္းေၾကာင့္ အိုမႈ ‘ဇရာ’၊ ေသမႈ ‘မရဏ’၊ စိုးရိမ္မႈ")]
+    [TestCase("ရဟန်းတို့ ဤတရားကို ပဋိစ္စသမုပ္ပါဒ်ဟု ဆိုအပ်၏။", "ရဟန္းတို႔ ဤတရားကို ပဋိစၥသမုပၸါဒ္ဟု ဆိုအပ္၏။")]
+    public void ComplexZawgyiStrings_ConvertToUnicodeCorrectly(string unicodeText, string expectedZawgyiText)
+    {
+        Assert.That(Convert.ToZawgyi(unicodeText), Is.EqualTo(expectedZawgyiText));
+    }
+}
