7b0d40be943c0c594c65e9d48c9aafa921f39d3a
Saw Naw <waterchan@gmail.com>
2020-12-30 19:53:42 +0000

    Add quick and dirty UI

---
+++ ZawgyiToUnicode.FilenameConverter.QuickAndDirtyUI/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ZawgyiToUnicode.FilenameConverter.QuickAndDirtyUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                var result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !String.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+
+                }
+            }
+        }
+    }
+}
